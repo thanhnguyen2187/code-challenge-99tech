18eec0885dfb0914ba51eb6d4a0cff6f83df559d
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-09-13 17:02:32 +0700

    problem-5/feat: implement rudimentary migration

--- src/problem5/src/data-access.ts
+++ src/problem5/src/data-access.ts
@@ -2,6 +2,34 @@ import type { Database } from "better-sqlite3";
 import type { TodoItem } from "./types.js";
 
 export namespace DataAccess {
+  export function initialize(db: Database) {
+    const userVersion = db.pragma("user_version", { simple: true }) as number;
+    switch (userVersion) {
+      case 0: {
+        db.prepare(`
+          CREATE TABLE todo_item (
+            id TEXT PRIMARY KEY,
+            title TEXT NOT NULL,
+            description TEXT NOT NULL,
+            created_at INTEGER DEFAULT (strftime('%s','now')),
+            completed_at INTEGER DEFAULT NULL,
+            updated_at INTEGER DEFAULT NULL
+          )
+        `).run();
+        db.pragma("user_version = 1");
+        break;
+      }
+      case 1: {
+        console.info("DataAccess.initialize: nothing else to do");
+        break;
+      }
+      default: {
+        const message = "DataAccess.initialize: invalid version!";
+        throw new Error(message);
+      }
+    }
+  }
+
   export async function create(db: Database, item: TodoItem) {
     const statement = db.prepare(
       "INSERT INTO todo_items(title, description)" + " VALUES (?, ?)",
--- src/problem5/src/types.ts
+++ src/problem5/src/types.ts
@@ -2,6 +2,7 @@ export type TodoItem = {
   id: string;
   title: string;
   description: string;
+  createdAt: number | null;
   completedAt: number | null;
   updatedAt: number | null;
 };
