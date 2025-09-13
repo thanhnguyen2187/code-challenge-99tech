0acd498b6566550037574e37929efa99ccb89b67
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-09-13 15:27:20 +0700

    problem-4/feat: add edge case handling

--- src/problem4/src/sum-to-n.ts
+++ src/problem4/src/sum-to-n.ts
@@ -1,5 +1,9 @@
 export namespace SumToN {
   export function versionA(n: number): number {
+    if (n <= 1) {
+      return 1;
+    }
+
     let sum = 0;
     for (let i = 1; i <= n; i++) {
       sum += i;
@@ -8,12 +12,20 @@ export namespace SumToN {
   }
 
   export function versionB(n: number): number {
+    if (n <= 1) {
+      return 1;
+    }
+
     const numbers = Array.from(Array(n + 1).keys());
     const sum = numbers.reduce((acc, cur) => acc + cur, 0);
     return sum;
   }
 
   export function versionC(n: number): number {
+    if (n <= 1) {
+      return 1;
+    }
+
     return Math.round((n * (n + 1)) / 2);
   }
 }
