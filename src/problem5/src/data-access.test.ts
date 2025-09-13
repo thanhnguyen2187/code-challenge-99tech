91dfecb5c5b7bd8a46183125921964b7f4d84f70
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-09-13 20:35:48 +0700

    problem-5/test: add test for filters of `listAll`

--- src/problem5/src/data-access.test.ts
+++ src/problem5/src/data-access.test.ts
@@ -1,3 +1,4 @@
+/** biome-ignore-all lint/style/noNonNullAssertion: false negative */
 import Database from "better-sqlite3";
 import { expect, test } from "vitest";
 import { DataAccess } from "./data-access.js";
@@ -24,6 +25,54 @@ test("create and list successfully", () => {
   expect(DataAccess.TodoItem.listAll(db)).length(2);
 });
 
+test("list with filter", () => {
+  const db = new Database(":memory:");
+  DataAccess.initialize(db);
+
+  DataAccess.TodoItem.create(db, {
+    title: "title 1",
+    description: "description 1",
+  });
+  DataAccess.TodoItem.create(db, {
+    title: "title 2",
+    description: "description 2",
+  });
+  DataAccess.TodoItem.create(db, {
+    title: "title 3",
+    description: "description 3",
+  });
+  DataAccess.TodoItem.create(db, {
+    title: "special 4",
+    description: "special 4",
+  });
+
+  {
+    const items = DataAccess.TodoItem.listAll(db, "1");
+    expect(items).length(1);
+    expect(items[0]!.title).toEqual("title 1");
+  }
+  {
+    const items = DataAccess.TodoItem.listAll(db, "title");
+    expect(items).length(3);
+    expect(items[0]!.title).toEqual("title 1");
+    expect(items[1]!.title).toEqual("title 2");
+    expect(items[2]!.title).toEqual("title 3");
+  }
+  {
+    const items = DataAccess.TodoItem.listAll(db);
+    expect(items).length(4);
+    const itemSpecial = items[3]!;
+    itemSpecial.completedAt = Math.round(Date.now() / 1_000);
+
+    DataAccess.TodoItem.update(db, itemSpecial);
+
+    const itemsCompleted = DataAccess.TodoItem.listAll(db, undefined, true);
+    expect(itemsCompleted).length(1);
+    const itemsNotCompleted = DataAccess.TodoItem.listAll(db, undefined, false);
+    expect(itemsNotCompleted).length(3);
+  }
+});
+
 test("find one successfully", () => {
   const db = new Database(":memory:");
   DataAccess.initialize(db);
@@ -34,7 +83,6 @@ test("find one successfully", () => {
   };
   DataAccess.TodoItem.create(db, itemData);
   const items = DataAccess.TodoItem.listAll(db);
-  // biome-ignore lint/style/noNonNullAssertion: false negative TypeScript check
   const id = items[0]!.id;
 
   const itemFound = DataAccess.TodoItem.findOne(db, id);
@@ -53,7 +101,6 @@ test("update successfully", () => {
   };
   DataAccess.TodoItem.create(db, itemData);
   const items = DataAccess.TodoItem.listAll(db);
-  // biome-ignore lint/style/noNonNullAssertion: false negative TypeScript check
   const item = items[0]!;
   const newTitle = "title 1 modified";
   item.title = newTitle;
