f7619a450b5ba49831ea53993965aa1d30d95009
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-09-13 15:19:42 +0700

    problem-4/refactor: move functions into namespace
    Also improve convention.

--- src/problem4/src/sum-to-n.ts
+++ src/problem4/src/sum-to-n.ts
@@ -1,17 +1,19 @@
-export function sum_to_n_a(n: number): number {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum += i;
+export namespace SumToN {
+  export function versionA(n: number): number {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+    return sum;
   }
-  return sum;
-}
 
-export function sum_to_n_b(n: number): number {
-  const numbers = Array.from(Array(n + 1).keys());
-  const sum = numbers.reduce((acc, cur) => acc + cur, 0);
-  return sum;
-}
+  export function versionB(n: number): number {
+    const numbers = Array.from(Array(n + 1).keys());
+    const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+    return sum;
+  }
 
-export function sum_to_n_c(n: number): number {
-  return Math.round((n * (n + 1)) / 2);
+  export function versionC(n: number): number {
+    return Math.round((n * (n + 1)) / 2);
+  }
 }
